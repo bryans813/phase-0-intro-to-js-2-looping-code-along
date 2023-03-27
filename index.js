dad8ec53fb3574ad2ca4ab0a98369e1bc473095a
Bryan Sosa <bry.sosa@outlook.com>
2023-03-26 22:33:13 -0400

    index passed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function countDown(x){
+   var count = x;
+    while(count>=0){
+        console.log(count);
+        count--;
+    }
+}
+function writeCards(x, y){  
+    const newArray= []   
+    for(let i = 0 ; i < x.length; i++){
+        var message = `Thank you, ${x[i]}, for the wonderful ${y} gift!`;
+        newArray.push(message);
+    }   
+    return newArray; 
+}  
+    
